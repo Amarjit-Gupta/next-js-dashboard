5268deebd810781a3904c297f2ab455a6f96fb39
Amarjit <akg433787@gmail.com>
2026-01-06 15:58:30 +0530

    first-commit

--- app/Emailer/page.jsx
+++ app/Emailer/page.jsx
@@ -59,7 +59,7 @@ const Emailer = () => {
 
                 <div className="flex-1 grid grid-cols-2 gap-2">
                     <div className=" flex flex-col justify-center px-1 bg-gray-100 rounded-lg">
-                        <p className="text-gray-500">keyword:</p>
+                        <p className="text-gray-500">Keyword:</p>
                         <p className="text-xl">65455</p>
                     </div>
                     <div className=" flex flex-col justify-center px-1 bg-gray-100 rounded-lg">
