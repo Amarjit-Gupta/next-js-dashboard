d20383d426e7ce0ba8c9268ea95e2ba62d0d7582
Amarjit <akg433787@gmail.com>
2026-01-06 17:09:53 +0530

    first-commit

--- app/page.js
+++ app/page.js
@@ -126,8 +126,8 @@ import Total from "./Total/page";
 
 const App = () => {
   return (
-    <div className=" bg-gray-100 p-3">
-
+    // <div className="h-screen bg-gray-100 p-3 grid grid-rows-[0.2fr_1fr_0.5fr_1.2fr] gap-4">
+<div className="bg-gray-100 p-3 m-auto flex flex-col gap-2">
       {/* HEADER */}
       <div className=" bg-white rounded-xl flex items-center border border-gray-200">
         <DateTime />
