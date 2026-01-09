b5cb4680b9cb683a339f5834ee33f54170931f77
Amarjit <akg433787@gmail.com>
2026-01-09 12:12:51 +0530

    first-commit

--- app/HashTag/page.jsx
+++ app/HashTag/page.jsx
@@ -1,7 +1,7 @@
 const HashTag = () => {
     return (
         <div className="flex-1 p-1 bg-gray-100 rounded-lg">
-            <h3 className="font-semibold text-gray-500 py-0.5 text-center">Hashtags</h3>
+            <h3 className=" font-bold text-gray-500 py-0.5 text-center">Hashtags</h3>
             {/* <ul className="list-disc pl-5 text-xs lg:text-xl">
                 <li>Lorem ipsum dolor</li>
                 <li>Lorem ipsum dolor</li>
