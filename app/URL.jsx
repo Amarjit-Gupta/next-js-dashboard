2503d4ed06c35894a6b22386512fc8e3749b4497
Amarjit-Gupta <168736859+Amarjit-Gupta@users.noreply.github.com>
2026-01-12 13:38:19 +0530

    first-commit

--- app/URL.jsx
+++ app/URL.jsx
@@ -1,1 +1,3 @@
-export const backend_url = "http://172.16.31.175:8000";
+// export const backend_url = "http://172.16.31.175:8000";
+
+export const backend_url = "https://posttarsal-telma-areological.ngrok-free.dev";
