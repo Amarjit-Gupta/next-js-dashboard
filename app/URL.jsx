d0d4247c19ad7cc88511aef1c7205ee2dccebc56
Amarjit-Gupta <168736859+Amarjit-Gupta@users.noreply.github.com>
2026-01-12 14:40:17 +0530

    Update backend URL to new Cloudflare address

--- app/URL.jsx
+++ app/URL.jsx
@@ -1,3 +1,3 @@
 // export const backend_url = "http://172.16.31.175:8000";
 
-export const backend_url = "https://posttarsal-telma-areological.ngrok-free.dev";
+export const backend_url = "https://walter-microphone-limousines-mile.trycloudflare.com";
