63fa8fa3155e85a65cd8ec1ceece2661848ba012
Amarjit-Gupta <168736859+Amarjit-Gupta@users.noreply.github.com>
2026-01-13 14:48:39 +0530

    Update backend_url to new Cloudflare endpoint

--- app/URL.jsx
+++ app/URL.jsx
@@ -1,3 +1,5 @@
 // export const backend_url = "http://172.16.31.175:8000";
 
-export const backend_url = "https://walter-microphone-limousines-mile.trycloudflare.com";
+// export const backend_url = "https://walter-microphone-limousines-mile.trycloudflare.com";
+
+export const backend_url = "https://merchandise-mapping-obtain-direction.trycloudflare.com";
