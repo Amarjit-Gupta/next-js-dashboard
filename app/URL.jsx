7946e4d735e4c981a9e8db93d26684faf1343191
Amarjit-Gupta <168736859+Amarjit-Gupta@users.noreply.github.com>
2026-01-14 16:05:45 +0530

    Update URL.jsx

--- app/URL.jsx
+++ app/URL.jsx
@@ -2,4 +2,6 @@
 
 // export const backend_url = "https://walter-microphone-limousines-mile.trycloudflare.com";
 
-export const backend_url = "https://merchandise-mapping-obtain-direction.trycloudflare.com";
+// export const backend_url = "https://merchandise-mapping-obtain-direction.trycloudflare.com";
+
+export const backend_url = "https://often-external-component-seat.trycloudflare.com";
